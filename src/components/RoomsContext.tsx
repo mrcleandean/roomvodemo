d0a8b841a5230760d22d83e53a7b1cce987d1f27
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-07 16:21:30 -0600

    added copy edge case if no item exists

--- src/components/RoomsContext.tsx
+++ src/components/RoomsContext.tsx
@@ -14,7 +14,9 @@ const RoomsContextProvider = ({ children }: { children: React.ReactNode }) => {
     const [noEnvIndex, setNoEnvIndex] = useState<number>(0);
     useEffect(() => {
         if (!copied) return; // prevents errors on inital render and prevents reruns on timeout
-        navigator.clipboard.writeText(rooms[imgIndex].src);
+        if (rooms[imgIndex]) { // prevents errors if rooms.length === 0
+            navigator.clipboard.writeText(rooms[imgIndex].src);
+        }
         const timeoutId = setTimeout(() => {
             setCopied(false);
         }, COPY_TIMEOUT);
